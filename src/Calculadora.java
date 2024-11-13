ff32447c296f41c3d291dc821dd6a033397a91fc
marcolapcx <168039947+marcolapcx@users.noreply.github.com>
2024-11-13 20:13:48 -0300

    Calculator

---
+++ src/Calculadora.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+
+public class Calculadora {
+    private JButton Btn01;
+    private JButton Btn02;
+    private JButton Btn03;
+    private JButton Btn04;
+    private JButton Btn07;
+    private JButton Btn06;
+    private JButton Btn05;
+    private JButton Btn08;
+    private JButton Btn09;
+    private JButton Btn00;
+    private JButton BtnMais;
+    private JButton BtnMenos;
+    private JButton BtnMulti;
+    private JButton BtnDivi;
+    private JButton BtnIgual;
+    private JButton BtnApaga;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
